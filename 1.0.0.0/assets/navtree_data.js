3b1db997841eb6070ad1138348fad2525e48ad91
Olivier Favre <olivier@yakaz.com>
2014-01-29 16:20:44 +0100

    Documentation site for 1.0.0.0

---
+++ 1.0.0.0/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "com.scoreflex", "com/scoreflex/package-summary.html", [ [ "Classes", null, [ [ "ConnectivityReceiver", "com/scoreflex/ConnectivityReceiver.html", null, "v2" ], [ "Scoreflex", "com/scoreflex/Scoreflex.html", null, "v2" ], [ "Scoreflex.RequestParams", "com/scoreflex/Scoreflex.RequestParams.html", null, "v2" ], [ "Scoreflex.Response", "com/scoreflex/Scoreflex.Response.html", null, "v2" ], [ "Scoreflex.ResponseHandler", "com/scoreflex/Scoreflex.ResponseHandler.html", null, "v2" ], [ "ScoreflexGcmClient", "com/scoreflex/ScoreflexGcmClient.html", null, "v2" ], [ "ScoreflexView", "com/scoreflex/ScoreflexView.html", null, "v2" ] ]
+, "" ] ]
+, "v2" ] ]
+
+;
+
---
+++ 1.0.0.0/reference/lists.js
@@ -0,0 +1,11 @@
+var DATA = [
+      { id:0, label:"com.scoreflex", link:"com/scoreflex/package-summary.html", type:"package" },
+      { id:1, label:"com.scoreflex.ConnectivityReceiver", link:"com/scoreflex/ConnectivityReceiver.html", type:"class" },
+      { id:2, label:"com.scoreflex.Scoreflex", link:"com/scoreflex/Scoreflex.html", type:"class" },
+      { id:3, label:"com.scoreflex.Scoreflex.RequestParams", link:"com/scoreflex/Scoreflex.RequestParams.html", type:"class" },
+      { id:4, label:"com.scoreflex.Scoreflex.Response", link:"com/scoreflex/Scoreflex.Response.html", type:"class" },
+      { id:5, label:"com.scoreflex.Scoreflex.ResponseHandler", link:"com/scoreflex/Scoreflex.ResponseHandler.html", type:"class" },
+      { id:6, label:"com.scoreflex.ScoreflexGcmClient", link:"com/scoreflex/ScoreflexGcmClient.html", type:"class" },
+      { id:7, label:"com.scoreflex.ScoreflexView", link:"com/scoreflex/ScoreflexView.html", type:"class" }
+
+    ];
